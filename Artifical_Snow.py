dc074698b9830e9beefea60315fd90a0dcab974d
timluis <57617645+timluis@users.noreply.github.com>
2020-12-08 11:25:38 +0100

    Update Artifical_Snow.py

--- Artifical_Snow.py
+++ Artifical_Snow.py
@@ -1,1 +1,22 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
+import rasterio
+from rasterio.plot import show
+
+
+def normalize(array):
+    """Normalizes numpy arrays into scale 0.0 - 1.0"""
+    array_min, array_max = array.min(), array.max()
+    return ((array - array_min)/(array_max - array_min))
+
+def RGBnorm(raster):
+    """Returns a raster with RGB color normalized
+    raster: rasterio DatasetReader"""
+    color_dict = {color.name:raster.read(n+1) for n,color in enumerate(raster.colorinterp)}
+    for k,v in color_dict.items():
+      color_dict[k] = normalize(v)
+    rgb =  np.dstack(tuple(v for v in color_dict.values()))
+    rgb=rasterio.plot.reshape_as_raster(rgb)
+    return rgb
 
