80f91c26d1040ac5dba716180efc5667a905151e
timluis <tim.philipp1@gmx.de>
2020-12-09 08:44:58 +0000

    changed Artifical_Snow added croped sat_im

--- Artifical_Snow.py
+++ Artifical_Snow.py
@@ -10,13 +10,14 @@ def normalize(array):
     array_min, array_max = array.min(), array.max()
     return ((array - array_min)/(array_max - array_min))
 
-def RGBnorm(raster):
+def RGBnorm(raster,img_shape=True):
     """Returns a raster with RGB color normalized
     raster: rasterio DatasetReader"""
     color_dict = {color.name:raster.read(n+1) for n,color in enumerate(raster.colorinterp)}
     for k,v in color_dict.items():
       color_dict[k] = normalize(v)
     rgb =  np.dstack(tuple(v for v in color_dict.values()))
-    rgb=rasterio.plot.reshape_as_raster(rgb)
+    if img_shape=False:
+      rgb=rasterio.plot.reshape_as_raster(rgb)
     return rgb
 
